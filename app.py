2b1cc053943cd7b7072a83633b65e789d0bfb2b2
Ryanhangralim <ryanhangralim@gmail.com>
2023-09-23 17:41:41 +0800

    Update

--- app.py
+++ app.py
@@ -1,32 +1,48 @@
 import streamlit as st
 import pandas as pd
+from st_pages import Page, show_pages
+from PIL import Image
 
-def get_goal(choice, num):
-    goal = sdg.query(f"SELECT year, goal_{num}_score FROM sdg WHERE country = '{choice}'")
-    goal[f"goal_{num}_score"] = goal[f"goal_{num}_score"].astype(float)
-    return goal
+st.set_page_config(
+    page_title="Homepage",
+    page_icon="🏠",
+)
+
+show_pages(
+    [
+        Page("app.py", "Homepage", ":house:"),
+        Page("pages/details.py", "Details", "📊")
+    ]
+)
 
 sdg = st.experimental_connection('sdg_db', type='sql')
+
 st.write("""
 # SDGs Data Visualization 
 """)
 
-#User choicebox
-country = sdg.query('SELECT DISTINCT country FROM sdg;')
-choice = st.selectbox("Pick a country", country, index=73)
+img = Image.open("assets/Sustainable_Development_Goals.png")
+st.image(img)
 
+st.write("""
+## Comparison Chart 
+""")
 
-#test goal_1
-st.write("""Goal 1""")
-goal_1 = get_goal(choice, 1)
-st.dataframe(goal_1)
-st.bar_chart(goal_1, x='year', y='goal_1_score')
-st.line_chart(goal_1, x='year', y='goal_1_score')
+#Comparison input
+countries = sdg.query('SELECT DISTINCT country FROM sdg;')
+choice_1 = st.selectbox("Pick first country", countries, index=73)
+choice_2 = st.selectbox("Pick second country", countries, index=80)
 
-# year = st.slider("Pick a year", 2000, 2022)
-
-all = sdg.query(f"SELECT year, goal_1_score, goal_2_score, goal_3_score, goal_4_score, goal_5_score, goal_6_score, goal_7_score, goal_8_score, goal_9_score, goal_10_score, goal_11_score, goal_12_score, goal_13_score, goal_14_score, goal_15_score, goal_16_score, goal_17_score FROM sdg WHERE country = '{choice}';")
-for i in range(1, 18):
-    all[f"goal_{i}_score"] = all[f"goal_{i}_score"].astype(float)
-st.line_chart(all, x="year")
-st.dataframe(all)
+index_1 = sdg.query(f"SELECT year, sdg_index_score FROM sdg WHERE country = '{choice_1}';")
+index_2 = sdg.query(f"SELECT year, sdg_index_score FROM sdg WHERE country = '{choice_2}';")
+index_1["sdg_index_score"] = index_1["sdg_index_score"].astype(float)
+index_2["sdg_index_score"] = index_2["sdg_index_score"].astype(float)
+df = pd.DataFrame(
+    {
+        'Year': index_1["year"],
+        f'{choice_1}': index_1["sdg_index_score"],
+        f'{choice_2}': index_2["sdg_index_score"],
+    },
+    columns=['Year', f'{choice_1}', f'{choice_2}']
+)
+st.line_chart(df, x="Year", y=[f'{choice_1}', f'{choice_2}'])
---
+++ pages/details.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+
+sdg = st.experimental_connection('sdg_db', type='sql')
+sdgs = ["No Poverty", "Zero Hunger", "Good Health and Well-Being", "Quality Education", "Gender Equality", "Clean Water and Sanitation", "Affordable and Clean Energy", "Decent Work and Economic Growth", "Industry Innovation and Infrastructure", "Reduces Inequalities", "Sustainable Cities and Communities", "Responsible Consumption and Production", "Climate Action", "Life Below Water", "Life on Land", "Peace, Justice and Strong Insitutions", "Partnerships for the Goals"]
+
+def get_goal(choice, num):
+    goal = sdg.query(f"SELECT year, goal_{num}_score FROM sdg WHERE country = '{choice}'")
+    goal[f"goal_{num}_score"] = goal[f"goal_{num}_score"].astype(float)
+    return goal
+
+st.write("""
+# SDGs Data Visualization 
+""")
+
+#User choicebox
+country = sdg.query('SELECT DISTINCT country FROM sdg;')
+choice = st.selectbox("Pick a country", country, index=73)
+
+#all chart
+all = sdg.query(f"SELECT year, sdg_index_score, goal_1_score, goal_2_score, goal_3_score, goal_4_score, goal_5_score, goal_6_score, goal_7_score, goal_8_score, goal_9_score, goal_10_score, goal_11_score, goal_12_score, goal_13_score, goal_14_score, goal_15_score, goal_16_score, goal_17_score FROM sdg WHERE country = '{choice}';")
+for i in range(1, 18):
+    all[f"goal_{i}_score"] = all[f"goal_{i}_score"].astype(float)
+all["sdg_index_score"] = all['sdg_index_score'].astype(float)
+st.line_chart(all, x="year")
+st.dataframe(all, hide_index=True)
+
+#print index chart
+on = st.toggle("SDG Index Score", value=True)
+if on:
+    st.write("""SDG Index Score""")
+    goal = sdg.query(f"SELECT year, sdg_index_score FROM sdg WHERE country = '{choice}'")
+    goal["sdg_index_score"] = goal["sdg_index_score"].astype(float)
+    st.line_chart(goal, x='year', y='sdg_index_score')
+
+#printing single chart
+for i in range(1, 18):
+    on = st.toggle(f"Goal {i}: {sdgs[i-1]}", value=True)
+    if on:
+        st.write(f"""Goal {i} score""")
+        goal = get_goal(choice, i)
+        st.line_chart(goal, x='year', y=f'goal_{i}_score')
