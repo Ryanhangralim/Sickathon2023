adf9e62447d49e76eacf193961922eafc9e119e9
Ryanhangralim <ryanhangralim@gmail.com>
2023-09-28 19:44:19 +0800

    Removed reference.py

--- pages/reference.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.header('References', divider='rainbow')
-st.write("""
-## Dataset source
-### - https://www.kaggle.com/datasets/sazidthe1/sustainable-development-report
-""")
