2803cdf8731ba9c86c4255fd726c3c62a19204a5
Ryanhangralim <ryanhangralim@gmail.com>
2023-09-28 10:25:15 +0800

    Update goals chart

--- pages/details.py
+++ pages/details.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 sdg = st.experimental_connection('sdg_db', type='sql')
-sdgs = ["No Poverty", "Zero Hunger", "Good Health and Well-Being", "Quality Education", "Gender Equality", "Clean Water and Sanitation", "Affordable and Clean Energy", "Decent Work and Economic Growth", "Industry Innovation and Infrastructure", "Reduces Inequalities", "Sustainable Cities and Communities", "Responsible Consumption and Production", "Climate Action", "Life Below Water", "Life on Land", "Peace, Justice and Strong Insitutions", "Partnerships for the Goals"]
+sdgs = ["Goal 1 : No Poverty", "Goal 2 : Zero Hunger", "Goal 3 : Good Health and Well-Being", "Goal 4 : Quality Education", "Goal 5 : Gender Equality", "Goal 6 : Clean Water and Sanitation", "Goal 7 : Affordable and Clean Energy", "Goal 8 : Decent Work and Economic Growth", "Goal 9 : Industry Innovation and Infrastructure", "Goal 10 : Reduces Inequalities", "Goal 11 : Sustainable Cities and Communities", "Goal 12 : Responsible Consumption and Production", "Goal 13 : Climate Action", "Goal 14 : Life Below Water", "Goal 15 : Life on Land", "Goal 16 : Peace, Justice and Strong Insitutions", "Goal 17 : Partnerships for the Goals"]
 
 def get_goal(choice, num):
     goal = sdg.query(f"SELECT year, goal_{num}_score FROM sdg WHERE country = '{choice}'")
@@ -22,21 +22,19 @@ all = sdg.query(f"SELECT year, sdg_index_score, goal_1_score, goal_2_score, goal
 for i in range(1, 18):
     all[f"goal_{i}_score"] = all[f"goal_{i}_score"].astype(float)
 all["sdg_index_score"] = all['sdg_index_score'].astype(float)
-st.line_chart(all, x="year", width=1500)
+st.line_chart(all, x="year")
 st.dataframe(all, hide_index=True)
 
-#print index chart
-on = st.toggle("SDG Index Score", value=True)
-if on:
-    st.write("""SDG Index Score""")
-    goal = sdg.query(f"SELECT year, sdg_index_score FROM sdg WHERE country = '{choice}'")
-    goal["sdg_index_score"] = goal["sdg_index_score"].astype(float)
-    st.line_chart(goal, x='year', y='sdg_index_score')
+goals = st.multiselect("Select goals to view", sdgs, ["Goal 1 : No Poverty", "Goal 2 : Zero Hunger"])
+print(goals)
+print(len(goals))
 
-#printing single chart
-for i in range(1, 18):
-    on = st.toggle(f"Goal {i}: {sdgs[i-1]}", value=True)
-    if on:
-        st.write(f"""Goal {i} score""")
-        goal = get_goal(choice, i)
-        st.line_chart(goal, x='year', y=f'goal_{i}_score')
+#print index chart
+if(len(goals)>0):
+    for i in range(len(goals)):
+        index = sdgs.index(goals[i])
+        print(index)
+        st.write(f"""### {goals[i]}""")
+        goal = get_goal(choice, index+1)
+        print(goal)
+        st.line_chart(goal, x='year', y=f'goal_{index+1}_score')
