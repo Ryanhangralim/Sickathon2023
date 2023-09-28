808ae1728df399f49f9b553eff2d1a546402aa21
Ryanhangralim <ryanhangralim@gmail.com>
2023-09-28 16:38:29 +0800

    added reference.py

---
+++ pages/reference.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.header('References', divider='rainbow')
+st.write("""
+## Dataset source
+### - https://www.kaggle.com/datasets/sazidthe1/sustainable-development-report
+""")
